0a17e6c8561f5c5fbfd8842d35ca3f928859001f
Josias <87546094+Josyas@users.noreply.github.com>
2023-06-14 15:51:03 -0300

    categoria concluído
    Back-end;
    Front-End;

--- Venda-FrontEnd/src/api/categoria-get.js
+++ Venda-FrontEnd/src/api/categoria-get.js
@@ -24,11 +24,44 @@ document.addEventListener('DOMContentLoaded', function() {
             editButton.classList.add('btn', 'btn-primary');
             editButton.style.float = 'right';
             editButton.style.marginRight = '5px';
+            
+            editButton.addEventListener('click', function(){
+                exibirModelEditicao(categoriaResponse);
+            })
 
             const deleteButton = document.createElement('button');
             deleteButton.textContent = 'Apagar';
             deleteButton.classList.add('btn', 'btn-danger');
             deleteButton.style.float = 'right';
+
+            deleteButton.addEventListener('click', function(){
+                const idCategoria = {
+                    Id: categoriaResponse.IdCategoria
+                }
+
+                const confirmaApgar = confirm('Deseja excluir o item?');
+
+                if(confirmaApgar){
+                    fetch('https://localhost:7180/categoria/remove-categoria', {
+                        method: 'DELETE',
+                        headers: {'Content-Type': 'application/json'},
+                        body: JSON.stringify(idCategoria) 
+                    })
+                    .then(response => {
+                        if(response.ok){
+                            location.reload();
+                        }
+                        else{
+                            response.text();
+                        }
+                    })
+                    .then(erro => {
+                        if(erro){
+                            exibirMensagemErro(erro);
+                        }
+                    })
+                }
+            })
 
             const actionsCell = document.createElement('td');
             actionsCell.classList.add('action-buttons');
@@ -42,3 +75,55 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     })
 })
+
+function exibirModelEditicao(item){
+
+   document.getElementById('edit-nomeInput').value = item.Nome;
+
+   const modal = new bootstrap.Modal(document.getElementById('edit-modal'));
+   modal.show();
+
+   const salvarButton = document.getElementById('saveButton');
+   salvarButton.addEventListener('click', function(){
+        salvarEdicao(item);
+   })
+}
+
+function salvarEdicao(item) {
+    const id = item.IdCategoria;
+   const nome = document.getElementById('edit-nomeInput').value;
+   debugger
+   const categoriaAlterada = {
+        IdCategoria: id,
+        Nome: nome
+   }
+
+   fetch('https://localhost:7180/categoria/atualiza-cliente', {
+        method: 'PUT',
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(categoriaAlterada)
+   })
+   .then(response => {
+        if(response.ok){
+            location.reload();
+        }
+        else{
+            response.text();
+        }
+   })
+   .then(erro => {
+        if(erro){
+            exibirMensagemErro(erro);
+        }
+   })
+}
+
+function exibirMensagemErro(mensagem){
+    Swal.fire({
+      icon: 'error',
+      title: 'Erro',
+      text: mensagem,
+    });
+}
